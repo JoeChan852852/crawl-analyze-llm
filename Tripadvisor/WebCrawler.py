d03676de040ae9cab5a02e969fdada26f284d8e6
JoeChan852852 <155080702+JoeChan852852@users.noreply.github.com>
2025-12-18 14:27:42 +0800

    Add files via upload

--- Tripadvisor/WebCrawler.py
+++ Tripadvisor/WebCrawler.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 '''
 The following pair of functions has been implemented to start and stop the Surfshark VPN service using its native 
 Linux command-line interface. Since the Surfshark client operates at sudo level and will invariably request the 
@@ -23,16 +17,16 @@ import time
 password = "davidisdumb" # Hardcoded Sudo password
 
 def StartVPN():
-    # i = random.randint(0, 140) # Randomly selects a Surfshark server location from the available list
+    #i = random.randint(0, 140) # Randomly selects a Surfshark server location from the available list
     print("Starting VPN")
     command = "sudo surfshark-vpn attack"
-
+    
     # Spawn the process
     child = pexpect.spawn(command, encoding='utf-8')
 
     # Expect the password prompt
     child.expect('.*password.*:')
-
+        
     # Send the password
     child.sendline(password)
 
@@ -47,12 +41,12 @@ def StartVPN():
     time.sleep(1)
     # Expect the  prompt
     child.expect("Enter a number to select the VPN connection type. For default UDP, press ENTER")
-
+    
     child.sendline('ls -l')
 
     # Capture output until the process completes
     child.expect(pexpect.EOF)
-
+        
     # Print the output
     print(child.before)
     print("---------------------------------------------------------------")
@@ -86,7 +80,6 @@ chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 chrome_options.add_experimental_option('useAutomationExtension', False)  # Disable automation extension
 
 def HTML_get(target,HTML_name):
-    DownVPN()
     # Set up ChromeDriver service
     service = Service(ChromeDriverManager().install())
 
@@ -111,22 +104,36 @@ def HTML_get(target,HTML_name):
     Anything markedly below this threshold strongly suggests that we’ve been quietly shown the door by Cloudflare 
     (or similar) and are, regrettably, blocked.
     '''
-
     number_of_cookies = 0
     html_source = None
+    Bot_detect_flag = 0
     while number_of_cookies < 4: 
-        StartVPN()
+        
+        if Bot_detect_flag > 1:
+            DownVPN()
+            time.sleep(1)
+            StartVPN()
+        
+        
         driver.get(target)
-        time.sleep(1)
+        time.sleep(random.randint(1, 8))
         html_source = driver.page_source
         cookies = driver.get_cookies()
         number_of_cookies = len(cookies)
+        
         if number_of_cookies > 4:
             print(f"Cookies captured: {len(cookies)}")
+            Bot_detect_flag = 0
             # Save to file
             with open(HTML_name, "w", encoding="utf-8") as f:
                 f.write(html_source)
-        DownVPN()
+        else:
+            Bot_detect_flag = Bot_detect_flag + 1
+            print("Bot was detected !!! \n 有內鬼終止交易 !!!")
+            
+        
+        
+        
     driver.quit()
-    return(html_source)
+   
 
